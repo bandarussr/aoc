957aa7832b0f1450e5c3dae8da8118fe8606d800
Shashank Bandaru <shashanksr.bandaru@gmail.com>
2025-12-03 09:26:36 -0500

    d1p2

--- y25/d01/d01.py
+++ y25/d01/d01.py
@@ -22,4 +22,18 @@ def star1(test: bool=False) -> str:
 
 def star2(test: bool=False) -> str:
     input = read_input(1, test)
-    return ""
+
+    curr = DIAL_START
+    num_zero = 0
+
+    for line in input:
+        pol = 1 if line[0] == 'R' else -1
+        num = int(line[1:])
+
+        for _ in range(num):
+            curr = (curr + pol) % MOD
+
+            if curr == 0:
+                num_zero += 1
+
+    return str(num_zero)
